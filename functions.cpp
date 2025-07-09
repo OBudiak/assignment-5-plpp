8f3c84df3dc7aeafb6eeaaed43cad595010f2ddc
OBudiak <obudiak@kse.org.ua>
2025-07-09 14:43:56 +0300

    working with parsing by priority

---
+++ functions.cpp
@@ -0,0 +1,41 @@
+﻿//
+// Created by Fujitsu-H770 on 09.07.2025.
+//
+
+#include "functions.h"
+
+string specSymbols[] = {" ", "{", "}", "(", ")", ",", "="};
+
+int priorityPower[] = {6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 2, 2, 1, 1};
+string priorityCommands[] = {",", "=",
+                            "{", "}", "(", ")",
+                            "max", "min", "pow", "abs",
+                            "*", "/",
+                            "+", "-",
+                            "def", "var",};
+
+bool isNumber(const string& s) {
+    if (s.empty()) return false;
+    size_t i = 0;
+
+    if (s[0] == '-') i++;
+
+    bool hasDigit = false;
+    bool hasDot = false;
+
+    for (; i < s.size(); ++i) {
+        if (isdigit(s[i])) {
+            hasDigit = true;
+            continue;
+        }
+        return false;
+
+    }
+    return hasDigit;
+}
+
+bool isVar(const string& previous, const string& s) {
+    if (s.empty() || previous.empty()) return false;
+    if (previous != "var") return false;
+    return true;
+}
---
+++ functions.h
@@ -0,0 +1,18 @@
+﻿//
+// Created by Fujitsu-H770 on 09.07.2025.
+//
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <string>
+
+using namespace std;
+
+inline string specSymbols[];
+inline int priorityPower[];
+inline string priorityCommands[];
+
+bool isNumber(const std::string& s);
+bool isVar(const string& previous, const string& element);
+
+#endif //FUNCTIONS_H
